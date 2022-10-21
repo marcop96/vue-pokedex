72598567b165dd7ef742c23bb12dd6b467bdca9d
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-10-21 23:23:26 +0200

    add style to pokemon list

--- src/components/PokeDex.vue
+++ src/components/PokeDex.vue
@@ -1,52 +1,56 @@
 <!-- eslint-disable vue/require-v-for-key -->
 <template>
-  <div>
-    <div id="search-bar" class="form-control">
-      <div class="input-group place-content-center mt-6">
-        <input
-          type="text"
-          placeholder="Search a Pokemon"
-          class="input input-bordered w-96 bg-yellow-200"
-        />
-        <button class="btn btn-square bg-red-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-      </div>
+  <div id="search-bar" class="form-control">
+    <div class="input-group place-content-center mt-6">
+      <input
+        type="text"
+        placeholder="Search a Pokemon"
+        class="input input-bordered w-96 bg-yellow-200"
+      />
+      <button class="btn btn-square bg-red-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
     </div>
-
-    <div class="flex justify-start items-center mt-11 ml-5">
-      <div id="pokemon-list">
-        <n-scrollbar style="max-height: 500px">
-          <ul>
-            <li
-              v-for="pokemon in pokemons"
-              @click="
-                clickedPokemonURL = pokemon.url;
-                obtainPokemonInfo();
-              "
-            >
-              {{ pokemon.name }}
-            </li>
-          </ul>
-        </n-scrollbar>
-      </div>
-
-      <div
-        id="pokemon-info-container"
-        class="h-96 w-2/5 -4 mx-auto flex flex-col items-center"
+  </div>
+  <div class="flex items-center justify-start w-full">
+    <!-- <div class="justify-self-start w-32 mt-11 ml-5"> -->
+    <n-scrollbar
+      class="justify-self-start w-36"
+      style="max-height: 500px"
+      trigger="none"
+    >
+      <ul>
+        <li
+          class="text-xl hover:text-red-500 hover:font-extrabold hover:cursor-pointer"
+          v-for="pokemon in pokemons"
+          @click="
+            clickedPokemonURL = pokemon.url;
+            obtainPokemonInfo();
+          "
+        >
+          {{ pokemon.name }}
+        </li>
+      </ul>
+    </n-scrollbar>
+    <!-- </div> -->
+    <div class="">
+      <n-card
+        title="Pokemon"
+        class="bg-yellow-400 w-72 items-center"
+        size="small"
       >
         <div class="img">
           <img
@@ -69,16 +73,16 @@
             </li>
           </ul>
         </div>
-      </div>
+      </n-card>
     </div>
-
-    <!-- <PaginationSystem :number-of-pages="countPokemons"></PaginationSystem> -->
   </div>
+
+  <!-- <PaginationSystem :number-of-pages="countPokemons"></PaginationSystem> -->
 </template>
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from "vue";
-import { NScrollbar } from "naive-ui";
+import { NScrollbar, NCard } from "naive-ui";
 
 //Ref declares REACTIVE TYPES
 import type { Ref } from "vue";
@@ -97,9 +101,9 @@ import type { PokemonStats, ObtainPokemonResponse } from "@/types/types";
 const clickedPokemonURL = ref("");
 const nextPokemonURL: Ref<null | string> = ref("");
 const totalPokemonCount = ref();
-const countPokemons = computed(() => {
-  return Math.floor(totalPokemonCount.value / 20);
-});
+// const countPokemons = computed(() => {
+//   return Math.floor(totalPokemonCount.value / 20);
+// });
 
 const pokemons = ref();
 
